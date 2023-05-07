288d98be9f082a08f2b624d943837f9779cb6a29
Huw Roberts <huwrroberts@gmail.com>
2023-05-07 16:01:20 +0100

    Move stuff

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,39 @@ import React, { useEffect, useMemo, useRef, useState } from "react";
 import { Canvas, createPortal, extend, useFrame } from "@react-three/fiber";
 import { OrbitControls, PerspectiveCamera, shaderMaterial, useFBO, useTexture } from "@react-three/drei";
 
+const ColorMaterial = shaderMaterial(
+  { uTexture: new THREE.Texture(), uTime: 0 },
+  // vertex
+  /* glsl */ `
+		varying vec2 vUv;
+		void main() {
+			vUv = uv;
+			gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+		}
+	`,
+  // fragment
+  /* glsl */ `
+		uniform sampler2D uTexture;
+		varying vec2 vUv;
+		void main() {
+			vec4 texture = texture2D(uTexture, vUv);
+			gl_FragColor = vec4(texture);
+			gl_FragColor.r += 0.001;
+		}
+	`
+);
+
+extend({ ColorMaterial });
+
 const FBOScene = ({ ...props }) => {
-  const texture = useTexture("/plants.jpg");
+  useTexture("/plants.jpg", texture => {
+    meshPortal.current.material.uniforms.uTexture.value = texture;
+  });
 
-  const ColorMaterial = shaderMaterial(
-    { uTexture: new THREE.Texture(), uTime: 0 },
-    // vertex
-    /* glsl */ `
-			varying vec2 vUv;
-			void main() {
-				vUv = uv;
-				gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-			}
-		`,
-    // fragment
-    /* glsl */ `
-			uniform sampler2D uTexture;
-			varying vec2 vUv;
-			void main() {
-				vec4 texture = texture2D(uTexture, vUv);
-				gl_FragColor = vec4(texture);
-				gl_FragColor.r += 0.001;
-			}
-		`
-  );
-
-  extend({ ColorMaterial });
-
-  const [planeWidth, setPlaneWidth] = useState(1);
-  const [planeHeight, setPlaneHeight] = useState(1);
-
-  const textureMesh = useRef();
+  const meshDisplay = useRef();
+  const meshTextureA = useRef();
+  const meshTextureB = useRef();
+  const meshPortal = useRef();
   const cam = useRef();
   const scene = useMemo(() => {
     const scene = new THREE.Scene();
@@ -44,14 +46,16 @@ const FBOScene = ({ ...props }) => {
   let textureB = useFBO();
 
   const setPlaneSize = () => {
-    var distance = cam.current.position.z - textureMesh.current.position.z;
+    var distance = cam.current.position.z - meshPortal.current.position.z;
     var aspect = textureA.viewport.width / textureA.viewport.height;
     var vFov = (cam.current.fov * Math.PI) / 180;
-    setPlaneWidth(planeHeight * aspect);
-    setPlaneHeight(2 * Math.tan(vFov / 2) * distance);
+    let planeHeight = 2 * Math.tan(vFov / 2) * distance;
+    let planeWidth = planeHeight * aspect;
+    meshPortal.current.scale.y = planeHeight;
+    meshPortal.current.scale.x = planeWidth;
   };
 
-  useFrame(state => {
+  useFrame((state, delta) => {
     setPlaneSize();
     state.gl.setRenderTarget(textureB);
     state.gl.render(scene, cam.current);
@@ -59,7 +63,11 @@ const FBOScene = ({ ...props }) => {
     var t = textureA;
     textureA = textureB;
     textureB = t;
-    textureMesh.current.material.uniforms.uTexture.value = textureA.texture;
+    meshPortal.current.material.uniforms.uTexture.value = textureA.texture;
+
+    meshDisplay.current.rotation.z += delta;
+    meshTextureA.current.rotation.z += delta;
+    meshTextureB.current.rotation.z += delta;
   });
 
   useEffect(() => {
@@ -71,25 +79,25 @@ const FBOScene = ({ ...props }) => {
       {createPortal(
         <>
           <PerspectiveCamera ref={cam} position={[0, 0, 3]} />
-          <mesh ref={textureMesh} position={[0, 0, 0]}>
-            <planeGeometry args={[planeWidth, planeHeight]} />
-            <colorMaterial uTexture={texture} />
+          <mesh ref={meshPortal} position={[0, 0, 0]}>
+            <planeGeometry />
+            <colorMaterial />
           </mesh>
         </>,
         scene
       )}
       {/* DISPLAY */}
-      <mesh position={[-1.5, 0, 0]}>
+      <mesh ref={meshDisplay} position={[-1.5, 0, 0]}>
         <planeGeometry />
         <meshBasicMaterial map={textureB.texture} />
       </mesh>
       {/* TEXTURE B / FBO 1 */}
-      <mesh position={[0, 0, 0]}>
+      <mesh ref={meshTextureB} position={[0, 0, 0]}>
         <planeGeometry />
         <colorMaterial uTexture={textureB.texture} />
       </mesh>
-      {/* TEXTURE B / FBO 2 */}
-      <mesh position={[1.5, 0, 0]}>
+      {/* TEXTURE A / FBO 2 */}
+      <mesh ref={meshTextureA} position={[1.5, 0, 0]}>
         <planeGeometry />
         <colorMaterial uTexture={textureA.texture} />
       </mesh>
